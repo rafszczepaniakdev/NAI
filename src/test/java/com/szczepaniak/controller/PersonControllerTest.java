8bc53f708ee39e3cacde6a2695117a817b4f6fab
Rafal Szczepaniak <raf.szczepaniak.dev@gmail.com>
2017-10-12 22:08:33 +0200

    Simple Spring Boot aplication with PersonController

---
+++ src/test/java/com/szczepaniak/controller/PersonControllerTest.java
@@ -0,0 +1,41 @@
+package com.szczepaniak.controller;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import junit.framework.TestCase;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class PersonControllerTest extends TestCase {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	public void homePage() throws Exception {
+		mockMvc.perform(get("/index.html")).andExpect(content().string(containsString("Get your greeting")));
+	}
+
+	@Test
+	public void greeting() throws Exception {
+		mockMvc.perform(get("/person/greeting")).andExpect(content().string(containsString("Hello, World!")));
+	}
+
+	@Test
+	public void greetingWithUser() throws Exception {
+		mockMvc.perform(get("/person/greeting").param("name", "Rafal"))
+				.andExpect(content().string(containsString("Hello, Rafal!")));
+	}
+
+}
