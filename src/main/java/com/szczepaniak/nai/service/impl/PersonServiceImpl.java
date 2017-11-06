2e65881d0e0eaf3366a04e747ff0fc9e2a7d2bb2
Rafał Szczepaniak <raf.szczepaniak@gmail.com>
2017-11-06 13:24:02 +0100

    Added empty service

---
+++ src/main/java/com/szczepaniak/nai/service/impl/PersonServiceImpl.java
@@ -0,0 +1,10 @@
+package com.szczepaniak.nai.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.szczepaniak.nai.service.PersonService;
+
+@Service
+public class PersonServiceImpl implements PersonService {
+
+}
